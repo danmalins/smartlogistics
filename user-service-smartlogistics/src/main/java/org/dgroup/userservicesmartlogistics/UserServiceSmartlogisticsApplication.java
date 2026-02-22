85675180b4436cfa63bb4b1c8c7b1b0c80c38649
danma <danmalina2707@gmail.com>
2026-02-22 17:46:12 +0200

    created project structure and basic microservice user-service

---
+++ user-service-smartlogistics/src/main/java/org/dgroup/userservicesmartlogistics/UserServiceSmartlogisticsApplication.java
@@ -0,0 +1,13 @@
+package org.dgroup.userservicesmartlogistics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceSmartlogisticsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceSmartlogisticsApplication.class, args);
+    }
+
+}
---
+++ user-service-smartlogistics/src/test/java/org/dgroup/userservicesmartlogistics/UserServiceSmartlogisticsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dgroup.userservicesmartlogistics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceSmartlogisticsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
